fb4c67aff6db101e51d17b7358ce9b8f094cbd67
muzamilmemon0 <muzamil.memon315@gmail.com>
2023-08-25 12:26:54 +0500

    stable version

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+function AddTask({ onAdd }) {
+  const [text, setText] = useState("");
+  const [day, setDay] = useState("");
+  const [reminder, setReminder] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!text) {
+      alert("please add a task!");
+      return;
+    }
+    onAdd({ text, day, reminder });
+
+    setTask("");
+    setDay("");
+    setReminder(false);
+  };
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <div className="form-control">
+        <label>Task</label>
+        <input
+          type="text"
+          placeholder="Add Task"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+      </div>
+      <div className="form-control">
+        <label>Day & Time</label>
+        <input
+          type="text"
+          placeholder="Day & Time"
+          value={day}
+          onChange={(e) => setDay(e.target.value)}
+        />
+      </div>
+      <div className="form-control form-control-check">
+        <label>Set Reminder</label>
+        <input
+          type="checkbox"
+          checked={reminder}
+          value={reminder}
+          onChange={(e) => setReminder(e.currentTarget.checked)}
+        />
+      </div>
+      <input type="submit" className="btn btn-block" value="Save Task" />
+    </form>
+  );
+}
+
+export default AddTask;
